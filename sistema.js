d50f0cdb1e110035e491f089eaa20676b86d1c36
Engcompaulo <engcompaulo@gmail.com>
2018-08-03 11:22:54 -0300

    Aplicação de funções para arrays em JavaScript

--- sistema.js
+++ sistema.js
@@ -34,88 +34,85 @@ function Participante() {
     }
  
     function removerParticipante(email) {
-        for(var i=0; i < participantes.length; i++){
-            if(participantes[i].email == email ){
-               var removerParticipanteDoEmail = participantes.splice(i, 1);              
-            }
-        }     
+        participantes.forEach(function(participante, indice, pariticipanraASerRemovido){
+           if(participante.email === email){ 
+            pariticipanraASerRemovido.splice(indice);}
+        })
     }
  
     function buscarParticipantesPorNome(nome){
-        return participantes.filter(function(p){  return p.nome == nome;   })
+        return participantes.filter(function(participante){  
+            return participante.nome == nome;   })
     } 
  
     function buscarParticipantesPorSexo(sexo){
-        return participantes.filter(function(p){  return p.sexo == sexo;   })
+        return participantes.filter(function(participante){  
+            return participante.sexo == sexo;   })
     }
  
     function buscarParticipantesAprovados(){
-        return participantes.filter(function(p){  return p.aprovado == true })
+        return participantes.filter(function(participante){  
+            return participante.aprovado == true })
     }
    
     function buscarParticipantesReprovados(){
-        return participantes.filter(function(p){  return p.aprovado == false })
- 
- 
+        return participantes.filter(function(participante){  
+            return participante.aprovado == false })
     }
  
     function obterParticipante(email){
-        return participantes.find(function(p){ return p.email == email ;  });       
+        return participantes.find(function(participante){ 
+            return participante.email == email ;  });       
     }
    
     function adicionarNotaAoParticipante(email, nota){
-      for(var i=0; i < participantes.length; i++){
-            if(participantes[i].email === email ){
-                   participantes[i].nota = nota;
-                   if(nota >= 70){
-                    participantes[i].aprovado = true;
-                   }else{
-                     participantes[i].aprovado = false;
-                    }
-            }
-        }
-       
-    }
+      participantes.filter(function(participante){
+          if(participante.email == email){
+              participante.nota = nota;
+          }
+      })
+      participantes.forEach(function(participante){
+          if(participante.nota >= 70){
+              participante.aprovado = true;
+          }else{
+            participante.aprovado = false;
+          }
+      })
+          
+    } 
+    
  
     function obterMediaDasNotasDosParticipantes(){
-        
-      const somadorDeNotas = participantes.map(p => p.nota ).reduce(function(somadorNota, valorAtual){
-      return   somadorNota + valorAtual;
-
-       })
-       return somadorDeNotas/obterTotalDeParticipantes();
-        
+     const somadorDeNotas = participantes.map(p => p.nota ).reduce(function(somadorNota, valorAtual){
+           return   somadorNota + valorAtual;
+           })
+     return somadorDeNotas/obterTotalDeParticipantes();   
     }
    
-    function obterTotalDeParticipantes(){
-        return participantes.length;
+    function obterTotalDeParticipantes(){    
+        return participantes.length;    
     }
    
     function verificarSeParticipanteEstaAprovado(email){
-        return participantes.filter(function(p){  return p.aprovado == true && p.email == email});
-           
- 
+        return participantes.filter(function(p){  
+            return p.aprovado === true && p.email === email});
     }
   
     function obterQuantidadeDeParticipantesPorSexo(sexo){
-        var quantidadeDeParticipantesPorSexo= 0;
-        for(var i=0; i < participantes.length; i++){
-            if(participantes[i].sexo === sexo ){
-                quantidadeDeParticipantesPorSexo++;              
-            }
+        var cont = 0;
+        if(participantes.find(function(p){return p.sexo === sexo})){
+            cont++;
         }
-        return quantidadeDeParticipantesPorSexo;
-   
+        return cont;
     }
  
     function validarEmail(email){
         for(var i=0; i < participantes.length; i++){
-            if(participantes[i].email == email ) throw "email cadastrado";              
+            if(participantes[i].email === email ) throw "email cadastrado";              
         }   
         return true;
     }
- 
-  
+   
     return {
         adicionarParticipante,
         removerParticipante,
