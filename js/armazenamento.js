d4400e007f1577bc22dbcc0255ee87194e245558
Engcompaulo <engcompaulo@gmail.com>
2018-08-17 23:24:44 -0300

    Salvando no localstorager

--- js/armazenamento.js
+++ js/armazenamento.js
@@ -1,75 +1,103 @@
-function Armazenamento(key){
+
+$(function(){
+    var operacao = "A"; //"A"=Adição; "E"=Edição
+   var indice_selecionado = -1;
+   var tbParticipantes = localStorage.getItem("tbParticipantes");// Recupera os dados armazenados
+   tbParticipantes = JSON.parse(tbParticipantes); // Converte string para objeto
+    if(tbParticipantes == null) // Caso não haja conteúdo, iniciamos um vetor vazio
+       tbParticipantes = [];
+    function Adicionar(){
+       var part = GetParticipante("email", $("#email").val());
+        if(part != null){
+           alert("Código já cadastrado.");
+           return;
+       }
+       
+        var participante = JSON.stringify({
+           nome         : $("#nome").val(),
+           sobrenome    : $("#sobrenome").val(),
+           email        : $("#email").val(),
+           idade        : $("#idade").val(),
+           nota         : $("#nota").val(),
+           sexo : $("input[name='marcarSexo']:checked").val()
     
-    //iniciar Storage com um array vazio
-    if(window.localStorage.getItem(key) === null)
-        window.localStorage.setItem(key, "[]");
+           
+   
+       });
+        tbParticipantes.push(participante);
+       
+        localStorage.setItem("tbParticipantes", JSON.stringify(tbParticipantes));
+        alert("Registro adicionado.");
+       return true;
+   }
+    function Listar(){
+       $("#tblListar").html("");
+       $("#tblListar").html(
+           "<thead>"+
+           "   <tr>"+
+           "   <th scope='col'>Nome</th>"+
+           "   <th scope='col'>Sobrenome</th>"+
+           "   <th scope='col'>Email</th>"+
+           "   <th scope='col'>Idade</th>"+
+           "   <th scope='col'>Nota</th>"+
+           "   <th scope='col'>Sexo</th>"+
+           "   </tr>"+
+           "</thead>"+
+           "<tbody>"+
+           "</tbody>"
+           );
+         for(var i in tbParticipantes){
+           var part = JSON.parse(tbParticipantes[i]);
+             $("#tblListar tbody").append("<tr>"+
+                                        "	<td scope='row'>"+part.nome+"</td>" + 
+                                        "	<td scope='row'>"+part.sobrenome+"</td>" + 
+                                        "	<td scope='row'>"+part.email+"</td>" + 
+                                        "	<td scope='row'>"+part.idade+"</td>" + 
+                                        "	<td scope='row'>"+part.nota+"</td>" + 
+                                        "	<td scope='row'>"+part.sexo+"</td>" + 
+                                        "</tr>");
+                                        $("#tblListar tbody").append("<td><img src='./img/faviconedi.ico' alt='"+i+"' class='btnEditar'/><img src='./img/favicon.ico' alt='"+i+"' class='btnExcluir'/></td>");
+        }
+   }
 
-    function adicionar(item){
-        //obtem os itens salvos
-        var itens = deserializar();
-        //guarda o item, observe que o armazenamento não sabe de que item se trata
-        //poderia ser um Participante ou qualquer outro objeto. Só precisa mesmo ser um objeto.
-        itens.push(item);
-        //Guarda novamente o Array modificado
-        serializar(itens);
-    }
+   function Excluir(){
+       tbParticipantes.splice(indice_selecionado, 1);
+       localStorage.setItem("tbParticipantes", JSON.stringify(tbParticipantes));
+       alert("Registro excluído.");
+   }
 
-    function remover(propriedade, valor){
-        var itens = deserializar();
-        var indice = itens.findIndex((elemento) => {
-            return elemento[propriedade] === valor;
-        });
-        itens.splice(indice, 1);
-        serializar(itens);
-    }
-
-    function atualizar(item, propriedade) {
-        //obtem os itens salvos        
-        var itens = deserializar();
-        //procura o indice do objeto que possui a propriedade igual ao do item que estou passando
-        var indice = itens.findIndex((elemento) => {
-            return elemento[propriedade] === item[propriedade]
-        });
-        //substitui o item. existem outras formas de fazer isso, abstraia
-        itens[indice] = item;
-        //Guarda novamente o Array modificado
-        serializar(itens);
-    }
-
-    function obterItem(propriedade, valor) {
-        var itens = deserializar();
-        return itens.find((elemento) => {
-             return elemento[propriedade] === valor; }
-            );
-    }
-
-    function obterItens(propriedade, valor) {
-        var itens = deserializar();
-        return itens.find((elemento) => { 
-            return elemento[propriedade] === valor; 
-        })
-    }
-
-    function obterTodosOsItens(){
-        return deserializar();
-    }
-
-    function deserializar(){
-        var itensSerializados = window.localStorage.getItem(key);
-        return JSON.parse(itensSerializados);
-    }
-
-    function serializar(itens){
-        var itensSerializados = JSON.stringify(itens);
-        window.localStorage.setItem(key, itensSerializados);
-    }
-
-    return {
-        adicionar,
-        remover,
-        atualizar,
-        obterItem,
-        obterItens,
-        obterTodosOsItens
-    };
-}
+   function GetParticipante(propriedade, valor){
+       var part = null;
+       for (var item in tbParticipantes) {
+           var i = JSON.parse(tbParticipantes[item]);
+           if (i[propriedade] == valor)
+               part = i;
+       }
+       return part;
+   }
+    Listar();
+    $("#formulario").on("submit",function(){
+       if(operacao == "A")
+           return Adicionar();
+       //else
+       //	return Editar();		
+   });
+    $("#tblListar").on("click", ".btnEditar", function(){
+       operacao = "E";
+       indice_selecionado = parseInt($(this).attr("alt"));
+       var part = JSON.parse(tbParticipantes[indice_selecionado]);
+       $("#IdNome").val(part.nome);
+       $("#IdSobrenome").val(part.sobrenome);
+       $("#IdEmail").val(part.email);
+       $("#IdIdade").val(part.idade);
+       $("#IdNota").val(part.nota);
+       $("#IdSexo").val(part.sexo);
+       $("#IdEmail").attr("readonly","readonly");
+       $("#txtNome").focus();
+   });
+    $("#tblListar").on("click", ".btnExcluir", function(){
+       indice_selecionado = parseInt($(this).attr("alt"));
+       Excluir();
+       Listar();
+   });
+}); 
--- js/sistema.js
+++ js/sistema.js
@@ -1,131 +1,131 @@
-//Objeto Participante
 function Participante() {
     this.nome = "";
     this.sobrenome = "";
     this.email = "";
-    this.idade = 0
-    this.sexo = 0
-    this.nota = 0
-    this.aprovado = false
+    this.idade = 0;
+    this.sexo = 0;
+    this.nota = 0;
+    this.aprovado = false;
 }
 
+
 /***********************
- * Representa o sistema
- * Uma vez instanciado, deve-se usar essa mesma
- * instancia em todas as operações.
- */
+* Representa o sistema
+* Uma vez instanciado, deve-se usar essa mesma
+* instancia em todas as operações.
+*/
 function SistemaCadastro() {
 
-    //Onde os participantes ficarão armazenados
-    //O array foi excluido e o código foi todo adaptado para usar as funções do objeto armazenamento
-    const armazenamento = new Armazenamento("participantes");
+
+    var participantes = [];
+   // const guardar = new Armazenamento('participante'); 
 
     function adicionarParticipante(nome, sobrenome, email, idade, sexo) {
-
-        if(obterParticipante(email))
-            throw new Error("Participante já cadastrado!");
         
-        var p = new Participante();
-        p.nome = nome;
-        p.sobrenome = sobrenome;
-        p.email = email;
-        p.idade = idade;
-        p.sexo = sexo;
 
-        //com o objeto Participante instanciado, o sistema delega para o armazenamento a responsabilidade de guardar essas informações
-        armazenamento.adicionar(p);
+        if (obterParticipante(email)) {
+            throw new Error ("email cadastrado");
+           
+        }else{
+            var p = new Participante();
+            p.nome = nome;
+            p.sobrenome = sobrenome;
+            p.email = email;
+            p.idade = idade;
+            p.sexo = sexo;
+            participantes.push(p);
+            //guardar.salvarLocalstorager(p);
+            
+        }
+        
     }
+
     function removerParticipante(email) {
-        armazenamento.remover("email", email);
-    }
-	function atualizarParticipante(nome, sobrenome, email, idade, sexo, nota){
-
-		var participante = obterParticipante(email);
-        //aqui eu troco todas as informações antigas, gerando um novo objeto.
-        //existem outras formas mais adequadas de fazer isso, mas abstrai isso nesse momento
-        participante = {nome,sobrenome,email,idade,sexo};
+        participantes.forEach(function (participante, indice, arrayParticipante) {
+            if(obterParticipante(email) === participante) {          
+                arrayParticipante.splice(indice, 1);
+            }
+            return false
+        })
         
-        //fico consistente com a regra de negocio e utilizo uma função auxiliar para processar a nota.
-        //Veja, hoje isso é simples mas poderia ser bem mais complexo do que uma simples comparação
-        processarNotaDoParticipante(participante, nota);
 
-        //pego esse objeto modificado e passo para o armazenamento substituir.
-        //perceba que o sistema não faz ideia de como os dados são guardados.
-        //ISSO É ENCAPSULAMENTO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! \o/
-        //apenas passo o novo objeto e qual campo ele deve usar pra identificar.
-        //talvez vc tenha colocado o código no armazenamento de forma que ele sempre procure por email, não tem problema nenhum.
-        //eu fiz dessa forma pq achei mais legal. :)
-        armazenamento.atualizar(participante, "email");
-	}
-    function buscarParticipantesPorNome(nome){
-        return armazenamento.obterItens("nome", nome);
-    }    
-    function buscarParticipantesPorSexo(sexo){
-        return armazenamento.obterItens("sexo", sexo);        
     }
-    function buscarParticipantesAprovados(){
-        return armazenamento.obterItens("aprovado", true);
-    }
-    function buscarParticipantesReprovados(){
-        return armazenamento.obterItens("aprovado", false);
-    }
-    function obterParticipante(email){
-        //implemente o código necessário
-        return armazenamento.obterItem("email", email);        
-    }
-    function obterParticipantes(){
-        //implemente o código necessário
-        return armazenamento.obterTodosOsItens();
-    }
-	function processarNotaDoParticipante(participante, nota){
-		participante.nota = nota;
-		participante.aprovado = nota >= 70;
-	}
-    function adicionarNotaAoParticipante(email, nota){
 
-        var participante = armazenamento.obterItem("email", email);
-        processarNotaDoParticipante(participante, nota);
-        armazenamento.atualizar(participante, "email");
+    function buscarParticipantesPorNome(nome) {
+        return participantes.filter(function (participante) {
+            return participante.nome === nome;
+        })
     }
-    function obterMediaDasIdadesDosParticipantes(){
-        var participantes = armazenamento.obterTodosOsItens();
 
-        var total = participantes.reduce(function(somaDasNotas, participante){            
-            return somaDasNotas + participante.nota;
+    function buscarParticipantesPorSexo(sexo) {
+        return participantes.filter(function (participante) {
+            return participante.sexo === sexo;
+        });
+    }
+
+    function buscarParticipantesAprovados() {
+        return participantes.filter(function (participante) {
+            return participante.aprovado;
+        });
+    }
+
+    function buscarParticipantesReprovados() {
+        return participantes.filter(function (participante) {
+            return participante.aprovado === false
+        });
+    }
+
+    function obterParticipante(email) {
+        return participantes.find(function (participante) {
+            return participante.email === email;
+        });
+    }
+
+    function adicionarNotaAoParticipante(email, nota) {
+        participantes.filter(function (participante) {
+            if (participante.email === email && nota >= 70) {
+                participante.nota = nota;
+                participante.aprovado = true;
+            }
+        });
+
+    }
+
+    function obterMediaDasNotasDosParticipantes() {
+       var totalNotas =  participantes.reduce(function (valorAtualParaSomar, valorNotaParticipante) {
+            return valorAtualParaSomar + valorNotaParticipante.nota;
         }, 0);
+        return totalNotas/participantes.length;
+    }
 
-        return total / participantes.length;
+    function obterTotalDeParticipantes() {
+        return participantes.length;
     }
-    function obterTotalDeParticipantes(){
-        return armazenamento.obterTodosOsItens().length;
+
+    function verificarSeParticipanteEstaAprovado(email) {
+        if (obterParticipante(email).aprovado === true) {
+            return true;
+        }
     }
-    function verificarSeParticipanteEstaAprovado(email){
-        //implemente o código necessário
-        var participante = obterParticipante(email);
-        if(participante)
-            return participante.aprovado;        
+
+    function obterQuantidadeDeParticipantesPorSexo(sexo) {
+         return buscarParticipantesPorSexo(sexo).length;
     }
-    function obterQuantidadeDeParticipantesPorSexo(sexo){
-        //implemente o código necessário
-        var resultado = buscarParticipantesPorSexo(sexo);
-        return resultado.length;
-    } 
-	
+
+
 
     return {
         adicionarParticipante,
         removerParticipante,
-		atualizarParticipante,
         buscarParticipantesPorNome,
         buscarParticipantesPorSexo,
         buscarParticipantesAprovados,
         buscarParticipantesReprovados,
         obterParticipante,
-        obterParticipantes,
         adicionarNotaAoParticipante,
-        obterMediaDasIdadesDosParticipantes,
+        obterMediaDasNotasDosParticipantes,
         obterTotalDeParticipantes,
         verificarSeParticipanteEstaAprovado,
-        obterQuantidadeDeParticipantesPorSexo        
+        obterQuantidadeDeParticipantesPorSexo
     };
 }
